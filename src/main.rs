23bc0da73d4cb144abd97639d9b23eb7b8cbeb93
Jakub Sandomierz <sandomierzj@gmail.com>
2023-06-17 14:32:08 +0200

    Init enum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main () {
-    println!("Hello world")
+enum MENU_CHOICES {
+    GET_USERNAME = 0,
+    GET_LASTNAME = 1,
+    GET_AGE = 2,
+    CHECK_LEGAL = 3,
+    EXIT = 9
+}
+
+fn main() {
+    println!("Hello world!")
 }
